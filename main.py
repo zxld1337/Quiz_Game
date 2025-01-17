e7cf931259da4ab12bdd0b3894815ad0fa648f9b
Silkó Levente <slevi1001@gmail.com>
2025-01-17 19:04:37 +0100

    Main menu

--- main.py
+++ main.py
@@ -3,7 +3,21 @@ import tkinter as tk
 
 
 class QuizGameApp():
-    ...
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Quiz Game")
+
+        self.root.geometry("700x400")
+
+        self.welcome_label = tk.Label(root, text="Üdvözöllek a Quiz Játékomban!", font=("Arial", 16), pady=40)
+        self.welcome_label.pack()
+
+        self.start_button = tk.Button(root, text="Kezdés", font=("Arial", 14), command=self.start_quiz)
+        self.start_button.pack(pady=40)
+
+    def start_quiz(self):
+        ...
+        
 
 
 
